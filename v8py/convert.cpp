5be9af981828f6b0fcb8c0ad7797166ada9d1b8c
Alex Siryi <desertkun@gmail.com>
2017-10-30 15:06:28 +0200

    Added python methods support

--- v8py/convert.cpp
+++ v8py/convert.cpp
@@ -202,7 +202,7 @@ Local<Value> js_from_py(PyObject *value, Local<Context> context) {
         return hs.Escape(array);
     }
 
-    if (PyFunction_Check(value)) {
+    if (PyFunction_Check(value) || PyMethod_Check(value)) {
         py_function *templ = (py_function *) py_function_to_template(value);
         return hs.Escape(py_template_to_function(templ, context));
     }
